3f14a4427eb727e817b1d3cd71a2822bf0e26313
Tim Jacomb <tim.jacomb@hmcts.net>
2019-01-18 15:58:36 +0000

    Removed team specific config
    Due to moving to recommending cluster per team

--- create-cluster-role-setup.sh
+++ create-cluster-role-setup.sh
@@ -12,9 +12,6 @@ if [ -z "${BASE_NAME}" ]; then
 fi
 
 export DEVELOPERS_GROUP=fbd14e44-da3c-4305-8731-0060f56c296f
-export CMC_GROUP=94bfcae1-11e0-4723-bef8-e6f3925eb55e
-export PROBATE_GROUP=cb3b15bc-bd18-4d17-bc52-1f4dc77afdad
-export DIVORCE_GROUP=439c3560-c51b-4c3f-8523-c1cee0e3fe0d
 export AKS_CLUSTER_READER=3043c9ac-a03c-419c-89ac-cbe1e83d461d
 
 CLUSTER_ADMINS_GROUP="${BASE_NAME}-cluster-admins"
@@ -42,34 +39,8 @@ kubectl apply -f templates/substituted/developers-log-reader-binding.yaml
 envsubst < templates/view-binding.template.yaml > templates/substituted/view-binding.yaml
 kubectl apply -f templates/substituted/view-binding.yaml
 
-export TEAM_NAMES='cmc probate divorce'
-
-for TEAM_NAME in ${TEAM_NAMES}; do
-  NAMESPACE_EXISTS=$(kubectl get namespaces -o=jsonpath="{range .items[?(@.metadata.name=='${TEAM_NAME}')]}{.metadata.name}{'\n'}{end}")
-
-  if [ -z "${NAMESPACE_EXISTS}" ]; then 
-    echo "${TEAM_NAME} namespace doesn't exist, creating"
-    kubectl create namespace ${TEAM_NAME}
-
-  else
-    echo "${TEAM_NAME} namespace exists, skipping create"
-  fi 
-
-  echo "Adding view binding for team: ${TEAM_NAME}"
-  export TEAM_NAME=${TEAM_NAME}
-  export TEAM_GROUP=$(az ad group list --query  "[?displayName=='${TEAM_NAME}-developers'].objectId" -o tsv)
-  envsubst < templates/view-binding-team.template.yaml > templates/substituted/view-binding-${TEAM_NAME}.yaml
-  kubectl apply -f templates/substituted/view-binding-${TEAM_NAME}.yaml
-
-  if [ "${DEVELOPERS_EDIT_TEAM_SCOPED}" == "true" ]; then
-    echo "Developers edit access at ${TEAM_NAME} namespace enabled"
-    envsubst < templates/edit-binding-team.template.yaml > templates/substituted/edit-binding-${TEAM_NAME}.yaml
-    kubectl apply -f templates/substituted/edit-binding-${TEAM_NAME}.yaml
-  fi
-done
-
 if [ "${DEVELOPERS_EDIT_GLOBAL_SCOPED}" == "true" ]; then
-    echo "Developers edit access at global namespace enabled"
+    echo "Developers edit access at cluster level enabled"
     envsubst < templates/developers-edit-binding.template.yaml > templates/substituted/developers-edit-binding.yaml
     kubectl apply -f templates/substituted/developers-edit-binding.yaml
 fi
