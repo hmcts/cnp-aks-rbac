8108a85bdc11ff1453eab64ce0ed40a7d09add53
Tim Jacomb <tim.jacomb@hmcts.net>
2019-02-14 20:51:32 +0000

    Add issue link

--- create-aks-managed-identity.sh
+++ create-aks-managed-identity.sh
@@ -18,8 +18,7 @@ kubectl create -f https://raw.githubusercontent.com/Azure/aad-pod-identity/maste
 export NODE_RESOURCE_GROUP=$(az aks show -g core-infra-${BASE_NAME} --name ${BASE_NAME} --query nodeResourceGroup -o tsv)
 export IDENTITY_CLIENT_ID=$(az identity create -g ${NODE_RESOURCE_GROUP} -n ${BASE_NAME} --query clientId -o tsv)
 
-sleep 10 # previous command finishes before its actually created :troll: TODO report bug
-# failure is: No matches in graph database for '138d8747-9262-4a78-8842-4ae3e1682bbc'
+sleep 10 # previous command finishes before its actually created https://github.com/Azure/azure-cli/issues/8530
 
 export ARM_SUBSCRIPTION_ID=$(az account show --query id -o tsv)
 az role assignment create --role Reader --assignee ${IDENTITY_CLIENT_ID} --scope /subscriptions/${ARM_SUBSCRIPTION_ID}/resourcegroups/${NODE_RESOURCE_GROUP}
