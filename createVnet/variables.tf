de1d4c7ebf02512f65d7dc6e88cf7dd9b334a9f4
Tim Jacomb <tim.jacomb@hmcts.net>
2019-02-18 19:04:00 +0000

    Mostly working

--- createVnet/variables.tf
+++ createVnet/variables.tf
@@ -1,4 +1,10 @@
-variable "address_space" {}
+variable "address_space" {
+  default = "10.160.128.0/18"
+}
+variable "ag_subnet_address_space" {
+  default = "10.160.128.0/24"
+}
+
 
 variable "location" {
   default = "UK South"
